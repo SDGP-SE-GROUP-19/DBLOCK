c3749e609be00acc0ef317af7f5791630968f847
GavinFernando <gavinfernando172001@gmail.com>
2022-03-11 17:18:21 +0530

    admin signin under construction

--- frontEnd/LandFrontEnd/client/src/components/AdminNavigator.js
+++ frontEnd/LandFrontEnd/client/src/components/AdminNavigator.js
@@ -6,7 +6,7 @@ import SearchDeed2 from "./SearchDeed2";
 import ChangeLawSel from "./ChangeLawSel";
 import PageNotFound from "./PageNotFound";
 import './AdminNavigator.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 
 class AdminNavigator extends Component {
 
@@ -14,26 +14,24 @@ class AdminNavigator extends Component {
 
         return (
             <div className="AdminNavigator">
-                <Router>
+                
+                <nav className="navstyle">
+                    <Link to="/">[Home]</Link>
+                    <Link to="/NewDeedForm">[New Land Deed]</Link>
+                    <Link to="/SearchDeed">[Search Deeds]</Link>
+                    <Link to="/SearchDeed2">[Search Deeds By Address]</Link>
+                    <Link to="/ChangeLawSel">[Change Lawyer And Owner]</Link>
+                </nav>
 
-                    <nav className="navstyle">
-                        <Link to="/">[Home]</Link>
-                        <Link to="/NewDeedForm">[New Land Deed]</Link>
-                        <Link to="/SearchDeed">[Search Deeds]</Link>
-                        <Link to="/SearchDeed2">[Search Deeds By Address]</Link>
-                        <Link to="/ChangeLawSel">[Change Lawyer And Owner]</Link>
-                    </nav>
+                <Routes>
+                    <Route path="/" element={ <AdminHome /> }/>
+                    <Route path="/NewDeedForm" element={ <NewDeedForm /> }/>
+                    <Route path="/SearchDeed" element={ <SearchDeed /> }/>
+                    <Route path="/SearchDeed2" element={ <SearchDeed2 /> }/>
+                    <Route path="/ChangeLawSel" element={ <ChangeLawSel /> }/>
+                    <Route path="*" element={ <PageNotFound /> }/>
+                </Routes>
 
-                    <Routes>
-                        <Route path="/" element={ <AdminHome /> }/>
-                        <Route path="/NewDeedForm" element={ <NewDeedForm /> }/>
-                        <Route path="/SearchDeed" element={ <SearchDeed /> }/>
-                        <Route path="/SearchDeed2" element={ <SearchDeed2 /> }/>
-                        <Route path="/ChangeLawSel" element={ <ChangeLawSel /> }/>
-                        <Route path="*" element={ <PageNotFound /> }/>
-                    </Routes>
-
-                </Router>
             </div>
         );
     }
