8d33482afdc32ebfb17630943cddc689774ac83d
GavinFernando <gavinfernando172001@gmail.com>
2022-04-01 00:22:58 +0530

    commented on search deed css file

--- frontEnd/LandFrontEnd/client/src/components/PageNotFound.js
+++ frontEnd/LandFrontEnd/client/src/components/PageNotFound.js
@@ -4,6 +4,7 @@ class PageNotFound extends Component {
 
     render() {
 
+        // page which returns in case of an errornot found js is commented
         return (
             <div className="PageNotFound">
                 <h3>Error: page not found</h3>
