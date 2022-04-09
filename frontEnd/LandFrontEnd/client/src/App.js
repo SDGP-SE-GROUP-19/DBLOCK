1d5971752f13b3dd16782e22427cc0197e994871
Tharin Deelaka Mahale <tharin.20200709@iit.ac.lk>
2022-04-09 17:05:27 +0530

    commented out sign ins for testing

--- frontEnd/LandFrontEnd/client/src/App.js
+++ frontEnd/LandFrontEnd/client/src/App.js
@@ -14,6 +14,8 @@ class App extends Component {
       <div className="header">
       <Router>
         <AdminSignIn/>
+        {/* <LawyerSignIn /> */}
+        {/* <UserSignIn /> */}
       </Router>
 
       </div>
