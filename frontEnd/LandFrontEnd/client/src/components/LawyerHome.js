53d6a2518521bfaedca8f00e8673ea8e9509dfb5
ddnelaka <91384535+ddnelaka@users.noreply.github.com>
2022-04-21 17:06:06 +0530

    Added lawyer home page and css

--- frontEnd/LandFrontEnd/client/src/components/LawyerHome.js
+++ frontEnd/LandFrontEnd/client/src/components/LawyerHome.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
+import './Lawyer.css';
 
 class LawyerHome extends Component {
 
     render() {
         return (
-            <div>
-                <h1>Lawyer Home Page</h1>
+
+            <div class="body">
+                <div class="content">
+                    <h2><span class="lawyer">Lawyer</span>  <span class="Notification">Notification</span> Center</h2>
+                    <p> "Hello, this message is sent you to notify that your land is in transfering process..."</p>
+
+                    <span class="buttons">
+                        <button>ACCEPT</button>
+                        <button>DECLINE</button>
+                    </span>
+                </div>
+
             </div>
         );
     }
