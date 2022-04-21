a04623babdbd375a19b68b65fef348723a3b415f
GavinFernando <gavinfernando172001@gmail.com>
2022-04-21 08:22:42 +0530

    Added new user home page css file

--- frontEnd/LandFrontEnd/client/src/components/UserHome.js
+++ frontEnd/LandFrontEnd/client/src/components/UserHome.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import './AdminHome.css';
+import './UserHome.css';
 import profile from "./Images/admin.png";
 import { Link } from 'react-router-dom';
 
@@ -11,31 +11,33 @@ class UserHome extends Component {
         return (
             <>
             {/* column which holds the description of what this page is about */}
-                <div className="Row">
-                    <div className="Column">
-                        <h3 className="heading">Welcome User!</h3>
+                <div className="UserRow">
+                <div className="userhome-glass">
+                    <div className="UserColumn">
+                        <h2 className="userhome-heading">Welcome User !</h2>
                     </div>
 
-                    <div className="Column">
-                        <div className="AdminImage">
+                    <div className="UserColumn">
+                        <div className="userImage">
                             <img src={profile} alt="profile" className="profile"/>
                         </div>
                     </div>
 
                     {/* Container which holds a set of buttons that relate to the functionalities of the user */}
-                    <div className="containerH">
-                        <form className="formH">
+                    <div className="containerUH">
+                        <form className="formUH">
                         <div>
-                        <button className="AdminHomebtn"><Link to="/DeedsOfUser" style={{ textDecoration: 'none', color: 'white'}}>Deeds Assigned to User</Link></button>
+                        <button className="userhomebtn"><Link to="/DeedsOfUser" style={{ textDecoration: 'none', color: 'white'}}>Deeds Assigned to User</Link></button>
                         </div>
                         <div>
-                        <button className="AdminHomebtn"><Link to="/DeedHistory" style={{ textDecoration: 'none', color: 'white'}}>Deed History</Link></button>
+                        <button className="userhomebtn"><Link to="/DeedHistory" style={{ textDecoration: 'none', color: 'white'}}>Deed History</Link></button>
                         </div>
                         </form>
 
                     </div>
                     
                     
+                </div>
                 </div>
             </>
         );
