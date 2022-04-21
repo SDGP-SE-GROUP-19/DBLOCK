8aade855cea293988f7afde762f770a76cbab11f
GavinFernando <gavinfernando172001@gmail.com>
2022-04-21 08:42:59 +0530

    update class name in user navigation

--- frontEnd/LandFrontEnd/client/src/components/UserNavigator.js
+++ frontEnd/LandFrontEnd/client/src/components/UserNavigator.js
@@ -39,7 +39,7 @@ class UserNavigator extends Component {
                 {/* <h1>{ this.state.loginMessage }</h1> */}
                 {/* The navigation bar that routes through pages on a click of a button */}
                 <nav className="UserNavigator">
-                    <ul className="nav1">
+                    <ul className="nav">
                     <li className="Navbtn"><Link to="/" className="a" style={{ textDecoration: 'none', color: 'white'}}>Home</Link></li>
                     <li className="Navbtn"><Link to="/DeedsOfUser" className="a" style={{ textDecoration: 'none', color: 'white'}}>Deeds Assigned to User</Link></li>
                     <li className="Navbtn"><Link to="/DeedHistory" className="a" style={{ textDecoration: 'none', color: 'white'}}>Deed History</Link></li>
